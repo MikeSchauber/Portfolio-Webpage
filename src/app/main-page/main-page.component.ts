88493da0cb424cea98db88ccd582eee038afc7f1
Mike Schauber <mike.schauber96@gmail.com>
2024-08-28 11:39:08 +0200

    introduced menu bar and body style

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { AboveTheFoldComponent } from '../components/above-the-fold/above-the-fold.component';
-import { HeaderComponent } from '../shared/header/header.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, AboveTheFoldComponent],
+  imports: [ FooterComponent, AboveTheFoldComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
