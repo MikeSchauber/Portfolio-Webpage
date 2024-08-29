5f6245365e7972bbc61b5eaac0f4136f24b61fb1
Mike Schauber <mike.schauber96@gmail.com>
2024-08-29 14:20:53 +0200

    stretch banner div works fine

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 export const routes: Routes = [
     { path: '', component: MainPageComponent},
+    { path: 'aboutme', component: AboutMeComponent}
 ];
