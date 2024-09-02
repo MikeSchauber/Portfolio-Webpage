ccc1332f26c6dfd899a70eaf98222650f7fe5c5f
Mike Schauber <mike.schauber96@gmail.com>
2024-09-02 17:24:18 +0200

    generate new skills component

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -5,23 +5,26 @@ import { AboutMeComponent } from '../components/about-me/about-me.component';
 import { DialogComponent } from '../components/dialog/dialog.component';
 import { DialogDataService } from '../../services/dialog-data.service';
 import { CommonModule } from '@angular/common';
+import { SkillSetComponent } from '../components/skill-set/skill-set.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [CommonModule  , FooterComponent, AboveTheFoldComponent, AboutMeComponent, DialogComponent],
+  imports: [
+    CommonModule,
+    FooterComponent,
+    AboveTheFoldComponent,
+    AboutMeComponent,
+    SkillSetComponent,
+    DialogComponent,
+  ],
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.scss'
+  styleUrl: './main-page.component.scss',
 })
 export class MainPageComponent {
-  dialogOpen: boolean = false
+  dialogOpen: boolean = false;
 
   public dialogService = inject(DialogDataService);
 
-  constructor() {
-  
-  }
-
-
-
+  constructor() {}
 }
