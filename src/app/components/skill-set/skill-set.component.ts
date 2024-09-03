21fc5f3143084d306517f19d82ea69a788fb44f1
Mike Schauber <mike.schauber96@gmail.com>
2024-09-03 13:39:29 +0200

    implement translation in skillset

--- src/app/components/skill-set/skill-set.component.ts
+++ src/app/components/skill-set/skill-set.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-skill-set',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './skill-set.component.html',
   styleUrl: './skill-set.component.scss',
 })
@@ -15,6 +16,8 @@ export class SkillSetComponent {
   overWork: boolean = false;
   forward: boolean = false;
   interval: any[] = [];
+
+  constructor(public translate: TranslateService) {}
 
   animationForward() {
     this.forward = true;
