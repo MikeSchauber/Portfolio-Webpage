de28211fd630357c781b26e122fbda683c8cbb81
MikeSchauber <mike.schauber96@gmail.com>
2024-09-16 00:29:45 +0200

    add translation in projects component

--- src/app/components/portfolio/headline/headline.component.ts
+++ src/app/components/portfolio/headline/headline.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-headline',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './headline.component.html',
   styleUrl: './headline.component.scss'
 })
 export class HeadlineComponent {
+
+  constructor(public translate: TranslateService) {
+
+  }
 
 }
