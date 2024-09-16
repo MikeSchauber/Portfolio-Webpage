0d29ce0aa4775e6307c9aac7e4a6a73a1b601fd7
MikeSchauber <mike.schauber96@gmail.com>
2024-09-17 00:03:57 +0200

    setting html and scss code in formular component

--- src/app/components/formular/text/text.component.ts
+++ src/app/components/formular/text/text.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-text',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './text.component.html',
   styleUrl: './text.component.scss'
 })
 export class TextComponent {
+
+  constructor(public translate: TranslateService) {
+
+  }
 
 }
