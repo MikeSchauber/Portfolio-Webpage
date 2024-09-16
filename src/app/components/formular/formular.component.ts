a076c383f73bd85c0b6e26337d9e9cda50125a4b
MikeSchauber <mike.schauber96@gmail.com>
2024-09-16 22:17:10 +0200

    create base structur formular component

--- src/app/components/formular/formular.component.ts
+++ src/app/components/formular/formular.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { InputComponent } from './input/input.component';
+import { TextComponent } from './text/text.component';
 
 @Component({
   selector: 'app-formular',
   standalone: true,
-  imports: [],
+  imports: [InputComponent, TextComponent],
   templateUrl: './formular.component.html',
   styleUrl: './formular.component.scss'
 })
