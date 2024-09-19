9f5545a8ede78c5df113ba80f30a86287c9494d5
Mike Schauber <mike.schauber96@gmail.com>
2024-09-19 21:25:35 +0200

    BANNER ist working with SCSS Keyframe Animation

--- src/app/components/above-the-fold/banner/banner.component.ts
+++ src/app/components/above-the-fold/banner/banner.component.ts
@@ -1,5 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnInit,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
@@ -9,44 +15,54 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.scss',
 })
-export class BannerComponent implements OnInit {
-  banners = [
-    'banner.1',
-    'banner.2',
-    'banner.3',
+export class BannerComponent {
+  banners: string[] = [
+    'banner.1', 
+    'banner.2', 
+    'banner.3', 
     'banner.4',
     'banner.5',
     'banner.6',
     'banner.7',
-    'banner.8',
+    'banner.8'
   ];
+  
+  // positionSpeed: number = 0;
+  // transform: string = 'translateX(0px)';
+  // bannerWidth: number = 0;
+  // containerWidth: number = 0;
 
-  position_1: number = 0;
-  position_2: number = 0;
-  innerWidth!: number;
+  constructor(public translate: TranslateModule) { }
 
-  constructor(public translate: TranslateModule) {
-    this.innerWidth = window.innerWidth;
-    
-  }
+  // ngOnInit(): void {
+  //   this.getInnerWidth();
+  //   this.startBannerMovement();
+  // }
 
-  ngOnInit(): void {
-    this.position_1 = this.innerWidth;
-    this.position_2 = this.innerWidth * 2;
-    this.startBannerMovement();
-  }
+  // getInnerWidth(): void {
+  //   if (window.innerWidth <= 1100) {
+  //     this.containerWidth = window.innerWidth * 2;
+  //     this.positionSpeed = window.innerWidth;
+  //   } else {
+  //     this.containerWidth = window.innerWidth;
+  //     this.positionSpeed = window.innerWidth;
+  //   }
+  // }
 
-  startBannerMovement(): void {
-    const speed = 10;
-    setInterval(() => {
-      this.position_1 -= speed;
-      this.position_2 -= speed;
-      if (this.position_1 < -this.innerWidth) {
-        this.position_1 = this.innerWidth;
-      }
-      if (this.position_2 < -this.innerWidth) {
-        this.position_2 = this.innerWidth;
-      }
-    }, 1000 / 60);
-  }
+  // startBannerMovement(): void {
+  //   setInterval(() => {
+  //     this.positionSpeed -= 5;
+  //     this.transform = `translateX(${this.positionSpeed}px)`;
+
+  //     if (this.positionSpeed <= -this.containerWidth) {
+  //       this.positionSpeed += this.containerWidth * 2;
+  //     }
+  //   }, 1000 / 60);
+  // }
+
+
+
+  // getTransform(i: number) {
+  //   return this.transform;
+  // }
 }
