77a8a2ef58aa21a7de73964cffec1f5bb3255578
MikeSchauber <mike.schauber96@gmail.com>
2024-09-19 08:41:34 +0200

    banner becomes keyframe animation (doesnt work very well)

--- src/app/components/above-the-fold/banner/banner.component.ts
+++ src/app/components/above-the-fold/banner/banner.component.ts
@@ -15,44 +15,54 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.scss',
 })
-export class BannerComponent implements OnInit {
-  banners: string[] = ['banner.1', 'banner.2', 'banner.3', 'banner.4'];
-  positionSpeed: number = 0;
-  transform: string = 'translateX(0px)';
-  bannerWidth: number = 0;
-  containerWidth: number = 0;
+export class BannerComponent {
+  banners: string[] = [
+    'banner.1', 
+    'banner.2', 
+    'banner.3', 
+    'banner.4',
+    'banner.5',
+    'banner.6',
+    'banner.7',
+    'banner.8'
+  ];
+  
+  // positionSpeed: number = 0;
+  // transform: string = 'translateX(0px)';
+  // bannerWidth: number = 0;
+  // containerWidth: number = 0;
 
   constructor(public translate: TranslateModule) { }
 
-  ngOnInit(): void {
-    this.getInnerWidth();
-    this.startBannerMovement();
-  }
+  // ngOnInit(): void {
+  //   this.getInnerWidth();
+  //   this.startBannerMovement();
+  // }
 
-  getInnerWidth(): void {
-    if (window.innerWidth <= 1100) {
-      this.containerWidth = window.innerWidth * 2;
-      this.positionSpeed = window.innerWidth;
-    } else {
-      this.containerWidth = window.innerWidth;
-      this.positionSpeed = window.innerWidth;
-    }
-  }
+  // getInnerWidth(): void {
+  //   if (window.innerWidth <= 1100) {
+  //     this.containerWidth = window.innerWidth * 2;
+  //     this.positionSpeed = window.innerWidth;
+  //   } else {
+  //     this.containerWidth = window.innerWidth;
+  //     this.positionSpeed = window.innerWidth;
+  //   }
+  // }
 
-  startBannerMovement(): void {
-    setInterval(() => {
-      this.positionSpeed -= 0.7;
-      this.transform = `translateX(${this.positionSpeed}px)`;
+  // startBannerMovement(): void {
+  //   setInterval(() => {
+  //     this.positionSpeed -= 5;
+  //     this.transform = `translateX(${this.positionSpeed}px)`;
 
-      if (this.positionSpeed <= -this.containerWidth) {
-        this.positionSpeed += this.containerWidth * 2;
-      }
-    }, 1000 / 60);
-  }
+  //     if (this.positionSpeed <= -this.containerWidth) {
+  //       this.positionSpeed += this.containerWidth * 2;
+  //     }
+  //   }, 1000 / 60);
+  // }
 
 
 
-  getTransform(i: number) {
-    return this.transform;
-  }
+  // getTransform(i: number) {
+  //   return this.transform;
+  // }
 }
