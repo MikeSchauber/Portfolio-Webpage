b58f370dd7aa593ce216a52b5512f9f7f93f4c70
Mike Schauber <mike.schauber96@gmail.com>
2024-09-19 22:16:29 +0200

    improve all hover effects with scss instead of ts

--- src/app/components/skill-set/skill-set.component.ts
+++ src/app/components/skill-set/skill-set.component.ts
@@ -21,57 +21,10 @@ export class SkillSetComponent {
   mind: boolean = false;
 
   hover(movement: string) {
-    if (movement == "in") {
+    if (movement == 'in') {
       this.mind = true;
     } else {
       this.mind = false;
-    }
-
-  }
-
-  animationForward() {
-    this.forward = true;
-    this.setAnimation();
-  }
-
-  animationReverse() {
-    this.forward = false;
-    this.setAnimation();
-  }
-
-  setAnimation() {
-    if (this.forward) {
-      this.mouseOver();
-    } else {
-      this.mouseOut();
-    }
-  }
-
-  mouseOver() {
-    let interval = setInterval(() => {
-      this.positionSpeed -= 4;
-      this.roadPassed += 4;
-      this.checkAndSetPosition();
-      this.transformX = `translateX(${this.positionSpeed}px)`;
-    }, 1000 / 60);
-    this.interval.push(interval);
-  }
-
-  mouseOut() {
-    this.interval.forEach((e) => {
-      clearInterval(e);
-    });
-    this.positionSpeed = 0;
-    this.roadPassed = 0;
-    this.transformX = `translateX(0px)`;
-  }
-
-  checkAndSetPosition() {
-    if (this.roadPassed >= 300) {
-      this.positionSpeed = 0;
-    }
-    if (this.positionSpeed <= -150) {
-      this.positionSpeed = 150;
     }
   }
 }
