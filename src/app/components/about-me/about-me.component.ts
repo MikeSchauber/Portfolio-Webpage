df5181a5a0a06e26d1098d122c19f6e361a38497
Mike Schauber <mike.schauber96@gmail.com>
2024-09-24 12:06:22 +0200

    setting animation in about me section

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -8,9 +8,29 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss',
 })
-export class AboutMeComponent {
+export class AboutMeComponent implements AfterViewInit {
+  constructor(public translate: TranslateService, private elRef: ElementRef) {}
 
-  constructor(public translate: TranslateService) {
-
+  ngAfterViewInit(): void {
+    const animElements = this.elRef.nativeElement.querySelectorAll('.animElement');
+  
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        } else {
+          entry.target.classList.remove('visible');
+        }
+      });
+    });
+  
+    animElements.forEach((element: Element) => {
+      if (element instanceof HTMLElement) {  // Sicherstellen, dass das Element ein HTMLElement ist
+        observer.observe(element);
+      } else {
+        console.warn('Ungültiges Element für den Observer:', element);
+      }
+    });
   }
+  
 }
