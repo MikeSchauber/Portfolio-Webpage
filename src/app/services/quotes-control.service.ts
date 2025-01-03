2496fb2989373b58b2613921ffa40aa0806634d9
Mike Schauber <mike.schauber96@gmail.com>
2025-01-03 16:27:24 +0100

    Add a new Quote from Roman Kabukov

--- src/app/services/quotes-control.service.ts
+++ src/app/services/quotes-control.service.ts
@@ -5,58 +5,74 @@ import { Quote } from '../interfaces/quote';
   providedIn: 'root',
 })
 export class QuotesControlService {
+  /**
+   * Quotes are doubled because of the Animation
+   * Quote String comes out of i18n
+   */
   quotes: Quote[] = [
     {
-      position: 5,
+      position: 7,
       quote: 'quotes.quote_1',
-      autor: 'Jan Steiner - Frontend Developer'
+      autor: 'Jan Steiner - Frontend Developer',
+    },
+    {
+      position: 6,
+      quote: 'quotes.quote_2',
+      autor: 'Kai Schulz - Freelance Webdesigner',
+    },
+    {
+      position: 5,
+      quote: 'quotes.quote_3',
+      autor: 'Ertugrul K. - Frontend Developer',
     },
     {
       position: 4,
-      quote: 'quotes.quote_2',
-      autor: 'Kai Schulz - Freelance Webdesigner'
+      quote: 'quotes.quote_4',
+      autor: 'Roman Kabukov - Frontend Developer',
     },
     {
       position: 3,
-      quote: 'quotes.quote_3',
-      autor: 'Ertugrul K. - Frontend Developer'
+      quote: 'quotes.quote_1',
+      autor: 'Jan Steiner - Frontend Developer',
     },
     {
       position: 2,
-      quote: 'quotes.quote_1',
-      autor: 'Jan Steiner - Frontend Developer'
+      quote: 'quotes.quote_2',
+      autor: 'Kai Schulz - Freelance Webdesigner',
     },
     {
       position: 1,
-      quote: 'quotes.quote_2',
-      autor: 'Kai Schulz - Freelance Webdesigner'
+      quote: 'quotes.quote_3',
+      autor: 'Ertugrul K. - Frontend Developer',
     },
     {
       position: 0,
-      quote: 'quotes.quote_3',
-      autor: 'Ertugrul K. - Frontend Developer'
-    }
+      quote: 'quotes.quote_4',
+      autor: 'Roman Kabukov - Frontend Developer',
+    },
   ];
-  
-  
 
+  realQuotes: number[] = [0, 1, 2, 3];
 
-  realQuotes: number[] = [0, 1, 2];
-
+  /**
+   * Put more of the 240% values into the array if more quotes comes into it
+   */
   transform: string[] = [
     'translateX(-240%) scale(0.8)',
     'translateX(-120%) scale(0.8)',
     'translateX(0%)',
     'translateX(120%) scale(0.8)',
     'translateX(240%) scale(0.8)',
-    'translateX(360%) scale(0.8)',
+    'translateX(240%) scale(0.8)',
+    'translateX(240%) scale(0.8)',
+    'translateX(240%) scale(0.8)',
   ];
 
-  hide: boolean[] = [false, false, false, false, false, false];
+  hide: boolean[] = [false, false, false, false, false, false, false, false];
 
   activeQuote: number = 0;
 
   controlActive: boolean = false;
 
-  constructor() { }
+  constructor() {}
 }
