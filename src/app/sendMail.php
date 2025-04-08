62c7b9623fcf26279efeb697817a8560a3636627
Mike Schauber <mike.schauber96@gmail.com>
2025-04-08 19:10:29 +0200

    change sendMail.php recipient email

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'mike.schauber96@gmail.com';  
+            $recipient = 'kontakt@mikeschauber.de';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
